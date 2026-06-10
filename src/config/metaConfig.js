b87d5cc48573cbe7e40a4d63a04b1c6febd791e6
raosanjanaa <sanjanarao.9a@gmail.com>
2026-06-10 12:28:50 +0530

    meta config

--- src/config/metaConfig.js
+++ src/config/metaConfig.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 module.exports = {
   APP_ID: process.env.META_APP_ID,
   APP_SECRET: process.env.META_APP_SECRET,
-  GRAPH_VERSION: process.env.GRAPH_API_VERSION,
   FACEBOOK_REDIRECT_URI: process.env.FACEBOOK_REDIRECT_URI,
   INSTAGRAM_APP_ID: process.env.INSTAGRAM_APP_ID,
   INSTAGRAM_APP_SECRET: process.env.INSTAGRAM_APP_SECRET,
